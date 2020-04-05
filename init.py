d72f1dd11ce2b7828dfeb95711ce5930eb280d02
Aniket-Mohanty <50880199+Aniket-Mohanty@users.noreply.github.com>
2020-04-05 19:20:49 +0530

    Update init.py

--- init.py
+++ init.py
@@ -18,10 +18,10 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--batch_sz', type = int, default = 20, help = 'batch size for complete dataset')
-parser.add_argument('--cluster_batch_sz', type = int,default = 20, help = 'batch size for each cluster')
+parser.add_argument('--batch_sz', type = int, default = 80, help = 'batch size for complete dataset')
+parser.add_argument('--cluster_batch_sz', type = int, default = 20, help = 'batch size for each cluster')
 parser.add_argument('--dataset_name', default = 'SOP', help = 'name of dataset used')
-parser.add_argument('--dataset_path', default = './Stanford_Online_Products', help = 'path for input data')
+parser.add_argument('--dataset_path', default = '../Stanford_Online_Products', help = 'path for input data')
 
 parser.add_argument('--arch', default = 'resnet50', help = 'model architecture used')
 parser.add_argument('--pretrained', type = bool, default = True, help = 'Require pretrained model')
@@ -34,10 +34,10 @@ parser.add_argument('--decay', type = float, default = 0.0004, help = 'decay rat
 parser.add_argument('--tau', default = '30,35', help = 'milstones for multistepLR')
 parser.add_argument('--gamma', type = float, default = 0.3, help = 'gamma for multistepLR')
 
-parser.add_argument('--initial_epochs', type = int, default = 2, help = 'train full learner for initial_epochs')
-parser.add_argument('--num_epochs', type = int, default = 1, help = 'number of epochs')
+parser.add_argument('--initial_epochs', type = int, default = 5, help = 'train full learner for initial_epochs')
+parser.add_argument('--num_epochs', type = int, default = 20, help = 'number of epochs')
 parser.add_argument('--num_epochs_cluster', type = int, default =1, help = 'number of epochs per cluster')
-parser.add_argument('--num_T', type = int, default = 2, help = 'each cluster is trained num_T times before joining back learners')
+parser.add_argument('--num_T', type = int, default = 5, help = 'each cluster is trained num_T times before joining back learners')
 
 parser.add_argument('--loss_type', default = 'tripletloss', help = 'loss function used')
 parser.add_argument('--triplet_margin', type = float, default = 0.2, help = 'margin value for triplet loss')
@@ -52,14 +52,14 @@ parser.add_argument('--cluster_save', type = bool, default = True, help = 'use s
 
 parser.add_argument('--debug', type = bool, default = True, help = 'debug option')
 
-parser.add_argument('--save_model', type = bool, default = True, help = 'svae model?')
+parser.add_argument('--save_model', type = bool, default = True, help = 'save model?')
 parser.add_argument('--model_dict_path', default = './model_dict.pth', help = 'file in which model parameters are saved')
 parser.add_argument('--load_model', type = bool, default = False, help = 'load model parameters?')
 args = parser.parse_args()
 
 
 if(args.dataset_name == 'SOP'):
-    k_vals = [1,2,4,8]
+    k_vals = [1,10,100,1000]
 
 dataloaders = loader.give_dataloaders(args)
 
