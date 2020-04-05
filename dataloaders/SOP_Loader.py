309cf3b6c6126d106d0d43bd003f4905fd2a3986
Aniket-Mohanty <50880199+Aniket-Mohanty@users.noreply.github.com>
2020-04-05 19:14:12 +0530

    Update SOP_Loader.py

--- dataloaders/SOP_Loader.py
+++ dataloaders/SOP_Loader.py
@@ -8,49 +8,60 @@ Created on Thu Mar  5 10:57:12 2020
 import warnings
 warnings.filterwarnings("ignore")
 
-import numpy as np, os, sys, pandas as pd, csv, copy
-import torch, torch.nn as nn, matplotlib.pyplot as plt, random
+import numpy as np, pandas as pd, copy
+import torch
 
 from torch.utils.data import Dataset
 from PIL import Image
 from torchvision import transforms
 
-# enable for Windows
-# from multiprocessing import freeze_support
 
-def give_dataloaders(source_path, arch = 'resnet50', bs = 80, nb_kernels = 0,samples_per_class = 4):
+def give_dataloaders(args):
     """
-    Create a pytorch library comaptible dataloader for our code
+    Args:
+        dataset:     string, name of dataset for which the dataloaders should be returned.
+        source_path: path leading to dataset folder
+        arch:        network architecture used
+        bs:          batch size
+        nb_kernels:  number of workers for generating DataLoader
+    Returns:
+        dataloaders: dict of dataloaders for training, testing and evaluation on training.
     """
    
-    datasets = give_OnlineProducts_datasets(source_path,arch,samples_per_class)
+    datasets = give_OnlineProducts_datasets(args.dataset_path,args.arch,args.samples_per_class)
 
     #Move datasets to dataloaders.
     dataloaders = {}
     for key,dataset in datasets.items():
         is_val = dataset.is_validation
-        dataloaders[key] = torch.utils.data.DataLoader(dataset, batch_size=bs, num_workers=nb_kernels, shuffle=not is_val, pin_memory=True, drop_last=not is_val)
+        dataloaders[key] = torch.utils.data.DataLoader(dataset, batch_size=args.batch_sz, num_workers=args.nb_workers, shuffle=not is_val, pin_memory=True, drop_last=not is_val)
 
     return dataloaders
 
 
 def give_OnlineProducts_datasets(source_path,arch = 'resnet50',samples_per_class = 4):
     """
-    Generates training, testing, validation data for the given Stanford Online products dataset in a dictionary
+    This function generates a training, testing and evaluation dataloader for Metric Learning on the Online-Products dataset.
+    For Metric Learning, training and test sets are provided by given text-files, Ebay_train.txt & Ebay_test.txt.
+    So no random shuffling of classes.
+    Args:
+        arch: network architecture used
+    Returns:
+        dict of PyTorch datasets for training, testing and evaluation.
     """
     image_sourcepath  = source_path
-    
+    #Load text-files containing classes and imagepaths.
     training_files = pd.read_table(source_path+'/Ebay_train.txt', header=0, delimiter=' ')
     test_files     = pd.read_table(source_path+'/Ebay_test.txt', header=0, delimiter=' ')
 
-    
+    #Generate Conversion dict.
     conversion = {}
     for class_id, path in zip(training_files['class_id'],training_files['path']):
         conversion[class_id] = path.split('/')[0]
     for class_id, path in zip(test_files['class_id'],test_files['path']):
         conversion[class_id] = path.split('/')[0]
 
-    
+    #Generate image_dicts of shape {class_idx:[list of paths to images belong to this class] ...}
     train_image_dict, val_image_dict  = {},{}
     for key, img_path in zip(training_files['class_id'],training_files['path']):
         key = key-1
@@ -64,7 +75,7 @@ def give_OnlineProducts_datasets(source_path,arch = 'resnet50',samples_per_class
             val_image_dict[key] = []
         val_image_dict[key].append(image_sourcepath+'/'+img_path)
 
-    
+    ### Uncomment this if super-labels should be used to generate resp.datasets
     super_train_image_dict,super_conversion = {},{}
     for super_class_id, path in zip(training_files['super_class_id'],training_files['path']):
         conversion[super_class_id] = path.split('/')[0]
@@ -91,11 +102,22 @@ def give_OnlineProducts_datasets(source_path,arch = 'resnet50',samples_per_class
 
 class BaseTripletDataset(Dataset):
     """
-    Data augment class for mormalizing and resizing images to 224 for ResNet50 and imagenet standards
+    Dataset class to provide (augmented) correctly prepared training samples corresponding to standard DML literature.
+    This includes normalizing to ImageNet-standards, and Random & Resized cropping of shapes 224 for ResNet50 and 227 for
+    GoogLeNet during Training. During validation, only resizing to 256 or center cropping to 224/227 is performed.
     """
     def __init__(self, image_dict, arch = 'resnet50', samples_per_class=8, is_validation=False):
-       
-        
+        """
+        Dataset Init-Function.
+        Args:
+            image_dict:         dict, Dictionary of shape {class_idx:[list of paths to images belong to this class] ...} providing all the training paths and classes.
+            arch:               network architecture used
+            samples_per_class:  Number of samples to draw from one class before moving to the next when filling the batch.
+            is_validation:      If is true, dataset properties for validation/testing are used instead of ones for training.
+        Returns:
+            Nothing!
+        """
+        #Define length of dataset
         self.n_files     = np.sum([len(image_dict[key]) for key in image_dict.keys()])
 
         self.is_validation = is_validation
@@ -104,18 +126,19 @@ class BaseTripletDataset(Dataset):
 
         self.avail_classes    = sorted(list(self.image_dict.keys()))
 
-        
+        #Convert image dictionary from classname:content to class_idx:content, because the initial indices are not necessarily from 0 - <n_classes>.
         self.image_dict    = {i:self.image_dict[key] for i,key in enumerate(self.avail_classes)}
         self.avail_classes = sorted(list(self.image_dict.keys()))
 
-        
+        #Init. properties that are used when filling up batches.
         if not self.is_validation:
             self.samples_per_class = samples_per_class
+            #Select current class to sample images from up to <samples_per_class>
             self.current_class   = np.random.randint(len(self.avail_classes))
             self.classes_visited = [self.current_class, self.current_class]
             self.n_samples_drawn = 0
 
-        
+        #Data augmentation/processing methods.
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
         transf_list = []
         if not self.is_validation:
@@ -128,17 +151,21 @@ class BaseTripletDataset(Dataset):
         transf_list.extend([transforms.ToTensor(), normalize])
         self.transform = transforms.Compose(transf_list)
 
-        
+        #Convert Image-Dict to list of (image_path, image_class). Allows for easier direct sampling.
         self.image_list = [[(x,key) for x in self.image_dict[key]] for key in self.image_dict.keys()]
         self.image_list = [x for y in self.image_list for x in y]
 
-
+        #Flag that denotes if dataset is called for the first time.
         self.is_init = True
 
 
     def ensure_3dim(self, img):
         """
-        Ensure that the image is a 3D matrix
+        Function that ensures that the input img is three-dimensional.
+        Args:
+            img: PIL.Image, image which is to be checked for three-dimensionality (i.e. if some images are black-and-white in an otherwise coloured dataset).
+        Returns:
+            Checked PIL.Image img.
         """
         if len(img.size)==2:
             img = img.convert('RGB')
@@ -146,7 +173,12 @@ class BaseTripletDataset(Dataset):
 
 
     def __getitem__(self, idx):
-        
+        """
+        Args:
+            idx: Sample idx for training sample
+        Returns:
+            tuple of form (sample_class, torch.Tensor() of input image)
+        """
         if self.is_init:
             self.current_class = self.avail_classes[idx%len(self.avail_classes)]
             self.is_init = False
@@ -156,7 +188,9 @@ class BaseTripletDataset(Dataset):
                 return self.image_list[idx][-1], self.transform(self.ensure_3dim(Image.open(self.image_list[idx][0])))
 
             if self.n_samples_drawn==self.samples_per_class:
-               
+                #Once enough samples per class have been drawn, we choose another class to draw samples from.
+                #Note that we ensure with self.classes_visited that no class is chosen if it had been chosen
+                #previously or one before that.
                 counter = copy.deepcopy(self.avail_classes)
                 for prev_class in self.classes_visited:
                     if prev_class in counter: counter.remove(prev_class)
@@ -178,28 +212,15 @@ class BaseTripletDataset(Dataset):
 
 
 
-
-
-#source_path = '../../Stanford_Online_Products'
-#
-#dataloaders = give_dataloaders(source_path)
-
-#trainloader = iter(dataloaders['training'])
-#
-#def main():
-#     for i,(class_labels,image) in enumerate(trainloader,0):
-#         print('i:',i,'labels:',class_labels)
-#
-#if __name__ == '__main__':
-#     main()
-
 # if __name__ == '__main__':
-#     freeze_support() # enable for multiprocessing in Windows
+#     freeze_support()
 #     source_path = 'Stanford_Online_Products'
 #     dataloaders = give_dataloaders(source_path)
 #     trainloader = iter(dataloaders['training'])
 
+# for i,(class_labels,_) in enumerate(trainloader,0):
+#     print('i:',i,'labels:',class_labels)
 
-#     for i,(class_labels,_) in enumerate(trainloader,0):
-#         print('i:',i,'labels:',class_labels)
+
+
 
