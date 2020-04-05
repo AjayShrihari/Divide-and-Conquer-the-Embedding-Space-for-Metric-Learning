f9a42d4d4ac581895074684d8f654946494a8d4a
Aniket-Mohanty <50880199+Aniket-Mohanty@users.noreply.github.com>
2020-04-05 19:10:57 +0530

    Added init and utils

---
+++ init.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  5 15:25:07 2020
+
+@author: aniket
+"""
+import argparse
+
+import torch, torch.nn as nn
+
+import SOP_Loader as loader
+import model as net
+import train as train
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--batch_sz', type = int, default = 20, help = 'batch size for complete dataset')
+parser.add_argument('--cluster_batch_sz', type = int,default = 20, help = 'batch size for each cluster')
+parser.add_argument('--dataset_name', default = 'SOP', help = 'name of dataset used')
+parser.add_argument('--dataset_path', default = './Stanford_Online_Products', help = 'path for input data')
+
+parser.add_argument('--arch', default = 'resnet50', help = 'model architecture used')
+parser.add_argument('--pretrained', type = bool, default = True, help = 'Require pretrained model')
+
+parser.add_argument('--nb_workers', type = int, default = 0, help = 'Number of workers for dataloader')
+parser.add_argument('--samples_per_class', type = int, default = 4, help = 'Number of samples drawn per class while building dataloader')
+
+parser.add_argument('--lr', type = float, default = 0.00001, help = 'learning rate')
+parser.add_argument('--decay', type = float, default = 0.0004, help = 'decay rate for adam')
+parser.add_argument('--tau', default = '30,35', help = 'milstones for multistepLR')
+parser.add_argument('--gamma', type = float, default = 0.3, help = 'gamma for multistepLR')
+
+parser.add_argument('--initial_epochs', type = int, default = 2, help = 'train full learner for initial_epochs')
+parser.add_argument('--num_epochs', type = int, default = 1, help = 'number of epochs')
+parser.add_argument('--num_epochs_cluster', type = int, default =1, help = 'number of epochs per cluster')
+parser.add_argument('--num_T', type = int, default = 2, help = 'each cluster is trained num_T times before joining back learners')
+
+parser.add_argument('--loss_type', default = 'tripletloss', help = 'loss function used')
+parser.add_argument('--triplet_margin', type = float, default = 0.2, help = 'margin value for triplet loss')
+parser.add_argument('--sampling_type', default = 'semihard', help = 'type of sampling used')
+
+parser.add_argument('--num_learners', type = int, default = 8, help = 'number of learners embedding space is divided into')
+parser.add_argument('--embed_dim', type = int, default = 128, help = 'dimension of embedded space')
+
+parser.add_argument('--faiss_type', default = 'gpu', help = 'use cpu or gpu for faiss clustering')
+
+parser.add_argument('--cluster_save', type = bool, default = True, help = 'use save clusters method')
+
+parser.add_argument('--debug', type = bool, default = True, help = 'debug option')
+
+parser.add_argument('--save_model', type = bool, default = True, help = 'svae model?')
+parser.add_argument('--model_dict_path', default = './model_dict.pth', help = 'file in which model parameters are saved')
+parser.add_argument('--load_model', type = bool, default = False, help = 'load model parameters?')
+args = parser.parse_args()
+
+
+if(args.dataset_name == 'SOP'):
+    k_vals = [1,2,4,8]
+
+dataloaders = loader.give_dataloaders(args)
+
+model = net.ResNet50(args)
+if(torch.cuda.device_count() > 1):
+    model = nn.DataParallel(model)
+model.to(device)
+
+if(args.load_model):
+    model.load_state(torch.load(args.model_dict_path))
+
+train.train(args,model,dataloaders,k_vals)
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 01:17:30 2020
+
+@author: aniket
+"""
+import os
+import shutil
+import numpy as np
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset,DataLoader
+from sklearn.cluster import KMeans
+
+try:
+    import faiss
+    using_faiss = True
+except:
+    using_faiss = False
+    
+    
+def create_GpuIndex(d,ngpu = torch.cuda.device_count()):
+    
+    res = [faiss.StandardGpuResources() for i in range(ngpu)]
+
+    flat_config = []
+    for i in range(ngpu):
+        cfg = faiss.GpuIndexFlatConfig()
+        cfg.useFloat16 = False
+        cfg.device = i
+        flat_config.append(cfg)
+
+    if ngpu == 1:
+        index = faiss.GpuIndexFlatL2(res[0], d, flat_config[0])
+    else:
+        indexes = [faiss.GpuIndexFlatL2(res[i], d, flat_config[i]) for i in range(ngpu)]
+        index = faiss.IndexReplicas()
+        for sub_index in indexes:
+            index.addIndex(sub_index)
+    
+    return index
+
+class cluster_data(Dataset):
+    
+    def __init__(self,labels,images):
+        self.labels = labels
+        self.images = images
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self,idx):
+        return self.labels[idx],self.images[idx]
+    
+class cluster_saved_data(Dataset):
+    
+    def __init__(self,clus_id,labels):
+        self.clus_id = clus_id
+        self.labels = labels
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem(self,idx):
+        im = np.load('./clusters/cluster_' + str(self.clus_id) + '/im_' + str(idx+1) + '.npy')
+        trans = transforms.Compose([transforms.ToTensor()])
+        im = trans(im)
+        label = self.labels(idx)
+        return label,im
+
+def load_clusters(args,dloader,model):
+    
+    num_clusters = args.num_learners
+    batch_sz = args.cluster_batch_sz
+    num_workers = args.nb_workers
+    save = args.cluster_save
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    cluster_labels = [[] for _ in range(num_clusters)]
+    cluster_im = [[] for _ in range(num_clusters)]
+    cluster = [[] for _ in range(num_clusters)]
+    num_elements_in_cluster = np.zeros(num_clusters).astype('uint')
+
+    dloader.shuffle = False
+    
+    with torch.no_grad():
+        
+        target_labels, feature_coll = [],[]
+        iterator = iter(dloader)
+        
+        for idx,inp in enumerate(iterator):
+            input_img,target = inp[-1], inp[0]
+            target_labels.extend(target.numpy().tolist())
+            out = model(input_img.to(device))
+            feature_coll.extend(out.cpu().detach().numpy().tolist())
+            if(args.debug):
+                if(idx==99): break
+            
+        target_labels = np.hstack(target_labels).reshape(-1,1)
+        feature_coll  = np.vstack(feature_coll).astype('float32')
+        
+        if(using_faiss):
+            d = feature_coll.shape[-1]
+            if(args.faiss_type == 'gpu'):
+                cluster_index = create_GpuIndex(d = d)
+            else:
+                cluster_index = faiss.IndexFlatL2(d)
+                
+            kmeans            = faiss.Clustering(d, num_clusters)
+            kmeans.niter = 20
+            kmeans.min_points_per_centroid = 1
+            kmeans.max_points_per_centroid = 1000000000
+    
+            ### Train Kmeans
+            kmeans.train(feature_coll,cluster_index)
+            computed_centroids = faiss.vector_float_to_array(kmeans.centroids).reshape(num_clusters,d)
+    
+            ### Assign feature points to clusters
+            if(args.faiss_type == 'gpu'):
+                faiss_search_index = create_GpuIndex(d = d)
+            else:
+                faiss_search_index = faiss.IndexFlatL2(d)
+            
+            faiss_search_index.add(computed_centroids)
+            _, model_generated_cluster_labels = faiss_search_index.search(feature_coll, 1) 
+        else:
+            kmeans = KMeans(n_clusters = num_clusters, random_state=0).fit(feature_coll)
+            pred_labels = kmeans.labels_
+        
+        if(save):
+            if(os.path.isdir('./clusters')):
+                shutil.rmtree('./clusters')
+            os.mkdir('./clusters')
+            for i in range(num_clusters):
+                os.mkdir('./clusters/cluster_' + str(i))
+        
+        for idx,inp in enumerate(iterator):
+            sz = inp[0].numpy().shape[0]
+            for i in range(sz):
+                clus_id = pred_labels[i + idx*sz]
+                if(save):
+                    num_elements_in_cluster[clus_id] += 1
+                    np.save('./clusters/cluster_'+ str(clus_id) + '/im_' + str(num_elements_in_cluster[clus_id]) + '.npy', inp[1][i])
+                else:
+                    cluster_im[clus_id].append((inp[1][i]))
+                
+                cluster_labels[clus_id].append((inp[0][i]))
+            if(args.debug):
+                if(idx==99): break
+        
+        for i in range(num_clusters):
+            if(save):
+                data = cluster_saved_data(clus_id = i, labels = cluster_labels[i])
+            else:
+                data = cluster_data(cluster_labels[i],cluster_im[i])
+            cluster[i] = DataLoader(data,batch_size = batch_sz,shuffle = True,num_workers = num_workers)
+    
+    dloader.shuffle = True
+        
+    return cluster
