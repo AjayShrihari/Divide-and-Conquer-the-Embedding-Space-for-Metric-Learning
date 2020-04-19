6e67d2bda53297810c31746349121fd52e5b4b5d
Aniket-Mohanty <50880199+Aniket-Mohanty@users.noreply.github.com>
2020-04-19 16:45:54 +0530

    Update init.py

--- init.py
+++ init.py
@@ -10,7 +10,9 @@ import torch, torch.nn as nn
 
 sys.path.insert(1, 'dataloaders/')
 
-import SOP_Loader as loader
+import SOP_Loader as SOP_Loader
+import CUB_200_Loader as CUB_200_Loader
+import CARS_196_Loader as CARS_196_Loader
 import model as net
 import train as train
 
@@ -21,7 +23,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--batch_sz', type = int, default = 80, help = 'batch size for complete dataset')
 parser.add_argument('--cluster_batch_sz', type = int, default = 40, help = 'batch size for each cluster')
 parser.add_argument('--dataset_name', default = 'SOP', help = 'name of dataset used')
-parser.add_argument('--dataset_path', default = '../Stanford_Online_Products', help = 'path for input data')
 
 parser.add_argument('--arch', default = 'resnet50', help = 'model architecture used')
 parser.add_argument('--pretrained', type = bool, default = True, help = 'Require pretrained model')
@@ -44,6 +45,7 @@ parser.add_argument('--num_T', type = int, default = 10, help= 'each cluster is 
 
 parser.add_argument('--loss_type', default = 'tripletloss', help = 'loss function used')
 parser.add_argument('--triplet_margin', type = float, default = 0.2, help = 'margin value for triplet loss')
+parser.add_argument('--margin_beta', type = float, default = 1.2, help = 'Value of beta for margin loss')
 parser.add_argument('--sampling_type', default = 'semihard', help = 'type of sampling used')
 
 parser.add_argument('--num_learners', type = int, default = 4, help = 'number of learners embedding space is divided into')
@@ -63,6 +65,22 @@ args = parser.parse_args()
 
 if(args.dataset_name == 'SOP'):
     k_vals = [1,10,100,1000]
+    parser.add_argument('--dataset_path', default = '../Stanford_Online_Products', help = 'path for input data')
+    parser.add_argument('--num_classes', type = int, default = 11318,help = 'Number of classes in dataset. Default is num_classes in SOP train data')
+    args = parser.parse_args()
+    dataloaders = SOP_Loader.give_SOP_dataloaders(args)
+elif(args.dataset_name == 'CUB'):
+    k_vals = [1,2,4,8]
+    parser.add_argument('--dataset_path', default = '../CUB_200', help = 'path for input data')
+    parser.add_argument('--num_classes', type = int, default = 200,help = 'Number of classes in dataset. Default is num_classes in CUB 200 train data')
+    args = parser.parse_args()
+    dataloaders = CUB_200_Loader.give_CUB_dataloaders(args)
+elif(args.dataset_name == 'CARS'):
+    k_vals = [1,2,4,8]
+    parser.add_argument('--dataset_path', default = '../stanford-cars-dataset', help = 'path for input data')
+    parser.add_argument('--num_classes', type = int, default = 196,help = 'Number of classes in dataset. Default is num_classes in CARS 196 train data')
+    args = parser.parse_args()
+    dataloaders = CARS_196_Loader.give_CARS_dataloaders(args)
 
 dataloaders = loader.give_dataloaders(args)
 # print(len(dataloaders['training'].dataset))
